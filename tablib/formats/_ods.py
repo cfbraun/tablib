789b43be813b4be67ac369c4f68ccda391786037
Carl-Friedrich Braun <cfbraun@gerdes-ag.de>
2014-08-31 08:22:20 +0200

    Add ODS import

--- tablib/formats/_ods.py
+++ tablib/formats/_ods.py
@@ -12,6 +12,7 @@ else:
     from cStringIO import StringIO as BytesIO
 
 from tablib.compat import opendocument, style, table, text, unicode
+from xml.dom import Node
 
 title = 'ods'
 extensions = ('ods',)
@@ -98,3 +99,77 @@ def dset_sheet(dataset, ws):
                     cell = table.TableCell()
                     cell.addElement(text.P(text=col))
                     odf_row.addElement(cell)
+
+def _import_set(dset, sht, headers=True):
+    def getvalue(cell):
+        NUMERIC_TYPES = ('float', 'percentage', 'currency')
+
+        TYPE_VALUE_MAP = {
+            'string':     'stringvalue',
+            'float':      'value',
+            'percentage': 'value',
+            'currency':   'value',
+            'date':       'datevalue',
+            'time':       'timevalue',
+            'boolean':    'booleanvalue',
+            }
+
+        def convert(value, value_type):
+            if value is None:
+                pass
+            elif value_type in NUMERIC_TYPES:
+                value = float(value)
+            elif value_type == 'boolean':
+                value = True if value == 'true' else False
+            return value
+
+        def plaintext(cell):
+            t = []
+            for p in cell.getElementsByType(text.P):
+                t.extend([unicode(n.data) for n in p.childNodes if n.nodeType == Node.TEXT_NODE])
+            return '\n'.join(t)
+
+        t = cell.getAttribute('valuetype')
+        if  t is None:
+            result = None
+        elif t == 'string':
+            result = plaintext(cell)
+        else:
+            result = convert(cell.getAttribute(TYPE_VALUE_MAP[t]), t)
+        return result
+
+    dset.title = sht.getAttribute('name')
+    for i, row in enumerate(sht.getElementsByType(table.TableRow)):
+        values = []
+        for cell in row.getElementsByType(table.TableCell):
+            repeat = cell.getAttribute("numbercolumnsrepeated") or 1
+            values.extend([getvalue(cell)] * repeat)
+        if (i == 0) and headers:
+            dset.headers = values
+        else:
+            dset.append(values)
+
+def import_set(dset, in_stream, headers=True):
+    dset.wipe()
+
+    doc = opendocument.load(in_stream)
+    sht = doc.getElementsByType(table.Table)[0]
+    _import_set(dset, sht, headers)
+
+def import_book(dbook, in_stream, headers=True):
+    dbook.wipe()
+
+    doc = opendocument.load(in_stream)
+
+    xls_book = xlrd.open_workbook(file_contents=in_stream)
+    for sht in doc.getElementsByType(table.Table):
+        data = tablib.Dataset()
+        _import_set(data, sht, headers)
+        dbook.add_sheet(data)
+
+def detect(stream):
+    try:
+        opendocument.load(stream)
+        return True
+    except:
+        return False
